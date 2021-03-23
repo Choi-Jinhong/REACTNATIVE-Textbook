b76b223f7e7f0ccadd5a8a536f5ef381eb14a7a3
Gnong <jinhongdev@gmail.com>
2021-03-23 18:43:17 +0900

    코드 정렬

--- Practice01/Counter/src/App.tsx
+++ Practice01/Counter/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
-import Styled from "styled-components/native";
-import Counter from "./Screens/Counter";
+import React, {Fragment} from 'react';
+import Styled from 'styled-components/native';
+import Counter from './Screens/Counter';
 
 const Container = Styled.View`
   flex: 1;
@@ -12,7 +12,7 @@ const App = () => {
     <Container>
       <Counter title="This is a Counter App" initValue={5} />
     </Container>
-  )
-}
+  );
+};
 
 export default App;
