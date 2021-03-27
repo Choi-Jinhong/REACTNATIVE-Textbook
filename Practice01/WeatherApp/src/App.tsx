3e47d870582efb115559b8592c04e4aca7821c26
Gnong <jinhongdev@gmail.com>
2021-03-27 21:21:25 +0900

    Complete WeatherApp

--- Practice01/WeatherApp/src/App.tsx
+++ Practice01/WeatherApp/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Styled from 'styled-components/native';
 
-const App = () => {
-  
-}
+import WeatherView from '~/Screens/WeatherView';
+
+const Container = Styled.View`
+    flex: 1;
+    background-color: #EEE;
+`;
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <Container>
+      <WeatherView />
+    </Container>
+  );
+};
 
 export default App;
