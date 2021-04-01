bb874f7c1902062ca70d67ee85c719355beaf7ce
Gnong <jinhongdev@gmail.com>
2021-04-01 20:55:40 +0900

    Complete TodoList

--- Practice02/TodoList/src/App.tsx
+++ Practice02/TodoList/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Styled from 'styled-components/native';
+
+import { TodoListContextProvider } from '~/Context/TodoListContext';
+
+import Todo from './Screens/Todo';
+
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
 
 interface Props {};
 
 const App = ({}: Props) => {
   return (
-    <>
-    </>
+    <TodoListContextProvider>
+      <Container>
+        <Todo />
+      </Container>
+    </TodoListContextProvider>
   )
 }
 
